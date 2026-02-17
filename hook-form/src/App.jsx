9efb1a6717f0c9630f3c3096c69b611fd00673fa
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-02-17 23:24:20 +0530

    learn form validation

--- hook-form/src/App.jsx
+++ hook-form/src/App.jsx
@@ -2,34 +2,67 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useForm } from "react-hook-form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors ,isSubmitting },
+  } = useForm();
+
+  async function onSubmit(data) {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("submitting the form", data);
+  }
+
+
 
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)} >
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label> First Name</label>
+        <input
+          className={errors.firstName ? 'input-error' : ""}
+          {...register('firstName',
+            {
+              required: true,
+              minLength: { value: 3, message: "Min len atLeast 3" },
+              maxLength: { value: 6, message: "max length atmost 6" }
+            }
+          )} />
+        {errors.firstName && <p className='error-msg'>{errors.firstName.message}</p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <br />
+
+      <div>
+        <label> Middle Name</label>
+        <input
+          className={errors.middleName ? 'input-error' : ""}
+         
+            {...register('middleName',
+                {
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message: "middle Name is not as as per rules"
+                  }
+                }
+              )
+            } />
+            { errors.middleName && <p className='error-msg'>{errors.middleName.message}</p> }
+     </div>
+      <br />
+      <div>
+        <label> Last  Name</label>
+        <input {...register('lastName')} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <br />
+
+      <input type="submit" disabled={isSubmitting} 
+      value={isSubmitting ? 'submitting' : "submit"} />
+
+    </form>)
 }
 
 export default App
