bba46b756d51bc3dbb203e2bb8accbb8744e270e
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-03-01 15:58:06 +0530

    route the element

---
+++ pasteApp/paste-app/src/components/Paste.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Paste = () => {
+  return (
+    <div>
+      List of all paste
+    </div>
+  )
+}
+
+export default Paste
---
+++ pasteApp/paste-app/src/components/ViewPaste.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewPaste = () => {
+  return (
+    <div>
+      viewPaste
+    </div>
+  )
+}
+
+export default ViewPaste
